64bcbedea49e895c595547eb0196ef9583b161d4
Sophia Cheung <cheungsinhan@gmail.com>
2019-06-23 20:26:44 -0700

    finish some stuff

---
+++ frontend/calculator.jsx
@@ -0,0 +1,67 @@
+import React from 'react';
+
+class Calculator extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            result: 0,
+            num1: '',
+            num2: ''
+
+        }
+        this.setNum1 = this.setNum1.bind(this);
+        this.setNum2 = this.setNum2.bind(this);
+        this.add = this.add.bind(this);
+        this.subtract = this.subtract.bind(this);
+        this.multiply = this.multiply.bind(this);
+        this.divide = this.divide.bind(this);
+
+    }
+
+    render(){
+        return (
+            <div>
+                <h1>{this.state.result}</h1>
+                <input onChange={this.setNum1} type="text" value={this.state.num1}/>
+                <input onChange={this.setNum2} type="text" value={this.state.num2}/>
+                <br/>
+                <button onClick={this.add}>+</button>
+                <button onClick={this.subtract}>-</button>
+                <button onClick={this.multiply}>*</button>
+                <button onClick={this.divide}>/</button>
+            </div>
+        );
+    }
+
+    setNum1(e){
+        const num1 = e.target.value ? parseInt(e.target.value) : "";
+        this.setState({num1});
+    }
+
+    setNum2(e){
+        const num2 = e.target.value ? parseInt(e.target.value) : "";
+        this.setState({num2});
+    }
+
+    add(){
+        const result = this.state.num1 + this.state.num2;
+        this.setState({result});
+    }
+
+    subtract(){
+        const result = this.state.num1 - this.state.num2;
+        this.setState({ result });
+    }
+
+    multiply(){
+        const result = this.state.num1 * this.state.num2;
+        this.setState({ result });
+    }
+
+    divide(){
+        const result = this.state.num1 / this.state.num2;
+        this.setState({ result });
+    }
+} 
+
+export default Calculator;
---
+++ frontend/entry.jsx
@@ -0,0 +1,8 @@
+import Calculator from './calculator';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+document.addEventListener('DOMContentLoaded', () => {
+    const root = document.getElementById('root');
+    ReactDOM.render(<Calculator />, root);
+});
